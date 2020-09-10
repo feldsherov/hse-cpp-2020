f2f27025b2e5a507575e3725c2da9ccdc5fbbbde
Feldsherov Svyatoslav <feldsherov@me.com>
2020-09-10 20:12:22 +0300

    add comment

--- sem1/1_hse_magic/main.cpp
+++ sem1/1_hse_magic/main.cpp
@@ -6,7 +6,23 @@ bool has_special_symbols(const std::string_view s) {
   return s.find_first_of(special_symbols) != std::string::npos;
 }
 
+void foo(std::string& s) {
+  s[0] = 'l';
+  std::cout << s << std::endl;
+}
+
+void baz(std::string& s) {
+  std::cout << s << std::endl;
+}
+
 int main() {
+  char* cs = "kek";
+  // https://en.cppreference.com/w/cpp/string/basic_string/basic_string 
+  // basic_string( const CharT* s,
+  //          const Allocator& alloc = Allocator() );
+  // constructor works
+  foo(cs);
+  baz(cs);
   std::cout << has_special_symbols("факультет компьютерных наук") << std::endl;
   return 0;
 }
