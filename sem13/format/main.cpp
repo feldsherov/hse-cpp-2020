a129309febb9ca5050dd695ff23d1b67e740588e
Feldsherov Svyatoslav <feldsherov@me.com>
2020-10-16 09:02:32 +0300

    add comments

--- sem13/format/main.cpp
+++ sem13/format/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <format>
 
+// Not supported by any compiler :(
+// https://en.cppreference.com/w/cpp/20v
+
 int main() {
   std::string message = std::format("The answer is {}.", 42);
   std::cout << message << std::endl;
 
 
+  // What is std::ostream_iterator?
+  // What is std::back_inserter?
   std::format_to(
       std::ostream_iterator<double>(std::cout, " "), 
       "The answer is {}.", 42);
